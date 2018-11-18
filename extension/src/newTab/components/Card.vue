a21f2eafc402e20edef4d1624541050db1968548
Jyun-Han Lin <jyunhanlin@gmail.com>
2018-11-18 18:42:54 +0800

    [extension] add time, google search input, and card to newtab

---
+++ extension/src/newTab/components/Card.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card">
+    <header class="card__header">
+      <slot name="header"></slot>
+    </header>
+    <!-- <div class="card__hr" v-if="$slots.header"></div> -->
+    <main class="card__main">
+      <slot name="main"></slot>
+    </main>
+    <footer>
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  width: 100%;
+  height: 100%;
+  box-shadow: 0px 0px 8px 2px rgba(0,0,0,0.1);
+  border-radius: .8rem;
+  padding: 2rem 3rem;
+
+  &__header {
+    font-size: 1.8rem;
+  }
+
+  &__hr {
+    display: block;
+    border: .5px solid rgb(196, 196, 196);
+    margin-top: .5rem;
+    margin-bottom: 1rem;
+  }
+}
+</style>
--- extension/src/newTab/newTab.js
+++ extension/src/newTab/newTab.js
@@ -1,29 +1,19 @@
-import dayjs from 'dayjs';
-import { db } from '../services/firebase';
+import Vue from 'vue';
+import App from './App';
 import * as authService from '../services/auth';
 
+import './newTab.scss';
 
-document.getElementById('test').innerText = 'what the~~~';
-// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-//   switch (request.message){
-//       case "loadNewTab":
-//           console.log(request.data);
-//           //You have the object as request.data with tabUrl and tabHtml
-//           break;
-//   }
-// });
+global.browser = require('webextension-polyfill');
 
-const btn = document.getElementById('btn');
-console.log(btn);
+Vue.prototype.$browser = global.browser;
 
-authService.checkAuthStateChanged((user) => {
-  console.log(user);
-  db.collection(user.uid).doc(dayjs().format('YYYY-MM-DD'))
-    .onSnapshot((doc) => {
-      if (doc.exists) {
-        console.log('Document data:', doc.data());
-      }
-    }, (err) => {
-      console.log(err);
+let app;
+authService.checkAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      el: '#app',
+      render: h => h(App),
     });
+  }
 });
