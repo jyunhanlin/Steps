774697cd8b73e026ffb734c30b1381f1ef2b1700
Jyun-Han Lin <jyunhanlin@gmail.com>
2018-11-19 11:33:47 +0800

    [extension] add the followings: 1. add vue-chartjs and vue-raidal-progress component 2. change the way to guard router for popup and newtab 3. minor change the layout

--- extension/src/newTab/newTab.js
+++ extension/src/newTab/newTab.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import App from './App';
-import * as authService from '../services/auth';
 
 import './newTab.scss';
 
@@ -8,12 +7,8 @@ global.browser = require('webextension-polyfill');
 
 Vue.prototype.$browser = global.browser;
 
-let app;
-authService.checkAuthStateChanged(() => {
-  if (!app) {
-    app = new Vue({
-      el: '#app',
-      render: h => h(App),
-    });
-  }
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App),
 });
