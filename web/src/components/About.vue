702df1700af61e466d2318c4115c2243f4b5fed1
iancheng0323 <34837971+iancheng0323@users.noreply.github.com>
2018-11-30 10:34:23 +0800

    Add files via upload

--- web/src/components/About.vue
+++ web/src/components/About.vue
@@ -6,15 +6,29 @@
       </header>
       <main class="about__main">
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ab culpa praesentium id aspernatur perspiciatis quis non voluptatem neque delectus officia excepturi quae laboriosam iusto nisi laborum, vero natus. Vel.
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos iusto distinctio vero voluptatibus ut cupiditate, quidem adipisci praesentium. Quae numquam incidunt itaque ad dolorem nisi, eum impedit adipisci illum error?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia alias asperiores recusandae temporibus nemo et, sed consequatur ad? Consequatur hic dolores corrupti voluptate quidem a minus minima quaerat non deserunt?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus, illo officia explicabo odit consequatur ipsum aspernatur consequuntur est voluptatum consectetur officiis numquam ducimus sint ea necessitatibus nisi impedit praesentium.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt excepturi laboriosam nulla expedita libero asperiores obcaecati nesciunt? Totam ea earum libero in eligendi ratione autem laboriosam sunt, consequatur placeat asperiores?
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, ut sunt voluptate corrupti cum ipsum ipsam vel architecto aperiam culpa eum deleniti suscipit unde praesentium hic rem molestiae maiores quos.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda debitis sunt nesciunt, distinctio quidem eum! Repudiandae modi qui sed atque nisi eveniet praesentium alias? Dolorum quia placeat corporis est veritatis.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil fugiat, dolor totam quibusdam nostrum cum optio? Culpa enim magni illum labore a mollitia voluptates, quas facilis dicta eaque nihil!
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum nihil velit ducimus nobis repudiandae inventore? Pariatur, aut cupiditate consequatur exercitationem dolores ducimus. Blanditiis, quidem non est nisi illo eos? Magnam.
+          Steps是一個輕量化的目標管理工具，讓你輕鬆的紀錄與管理每天的工作項目。
+          我們盡量將功能做的簡單直覺，因為我們每天的工作已經如此繁瑣，管理工具應該簡單點。<br><br>
+          Steps的主要功能與操作如下：<br>
+          <ul>
+            <li>-	新增事項：利用快捷鍵(alt+n)或點擊綠色加號</li>
+            <li>-	編輯事項：點擊您想編輯的事項</li>
+            <li>-	刪除事項：將滑鼠滑到該事項上，然後點擊右邊的刪除</li>
+            <li>-	改變事項順序：點擊事項清單右上角的圖示，切換為拖動模式</li>
+          </ul>
+          <br>          
+          Steps是一個不斷精進的項目。您在使用的當下，我們也正在規劃、開發新功能。如果您對Steps有任何的改善建議（或許願想要什麼功能），請在改善與建議告訴我們。<br><br>
+          最後，Steps是一個志願性質的項目，參與的人員羅列如下。很感謝所有對這個項目持續付出的人們：
+          <ul>
+            <li>- <a href="https://www.linkedin.com/in/iancheng0323/">鄭斐凡</a></li>
+            <li>- <a href="https://github.com/jyunhanlin">林均翰</a></li>
+          </ul>
+        </p>
+        <h2>Credits:</h2>
+        <p>
+          <img src="../stairs.png" alt="" style="margin-right:1rem">
+          <img src="../shuffle_light.png" alt="" style="margin-right:1rem">
+          <img src="../shuffle_dark.png" alt=""><div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+          <img src="../menu.png" alt=""><div>Icons made by <a href="https://www.flaticon.com/authors/chris-veigt" title="Chris Veigt">Chris Veigt</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
         </p>
       </main>
       <footer class="about__footer">
@@ -45,6 +59,12 @@
     height: 30rem;
     overflow: auto;
     margin: 1rem 0;
+    p{
+      font-size:1.4rem;
+    }
+    h2{
+      margin-top:1rem;
+    }
   }
 
   &__footer {
